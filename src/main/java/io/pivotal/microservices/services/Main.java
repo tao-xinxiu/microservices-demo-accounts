c1613cafa9bbc32cf2f76cd99db04163d0fafa73
tao-xinxiu <isabelle.tao@orange.com>
2016-12-29 13:45:00 +0100

    move accounts service codebase from microserivce-demo

---
+++ src/main/java/io/pivotal/microservices/services/Main.java
@@ -0,0 +1,9 @@
+package io.pivotal.microservices.services;
+
+import io.pivotal.microservices.services.accounts.AccountsServer;
+
+public class Main {
+	public static void main(String[] args) {
+		AccountsServer.main(args);
+	}
+}
